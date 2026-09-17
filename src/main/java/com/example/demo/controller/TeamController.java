c6b70d041d1da7ce1cb86fafcf4fdd7253f2697b
3030839 <3030839@30308-39>
2026-09-17 15:48:06 +0900

    ControllerでServiceをmodelに格納しました。

--- src/main/java/com/example/demo/controller/TeamController.java
+++ src/main/java/com/example/demo/controller/TeamController.java
@@ -1,15 +1,25 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.service.KanaiService;
+import com.example.service.KojiService;
+
 @Controller
 public class TeamController {
+	@Autowired private KanaiService kanaiService;
+	@Autowired private KojiService kojiService;
+	
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("leaderMsg", "リーダーです！");
         // TODO: ここにメンバーのServiceを後で呼ぶ
+        model.addAttribute("kanaiMsg",kanaiService.getProfile());
+        model.addAttribute("kojiService",kojiService.getProfile());
+        
         return "index";
     }
 }
