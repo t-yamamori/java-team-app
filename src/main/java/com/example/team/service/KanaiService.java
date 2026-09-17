e536410dc992ecba7afc69d967ad788a398a30ec
3030818 <3030818@30308-18>
2026-09-17 14:24:56 +0900

    金井が自己紹介を追加。レビューをお願いします。

---
+++ src/main/java/com/example/team/service/KanaiService.java
@@ -0,0 +1,15 @@
+package com.example.team.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class KanaiService {
+	
+
+	public class TanakaService {
+	    public String getProfile() {
+	        return "メンバーの田中です！趣味は趣味はドライブです。";
+	    }
+	}
+
+}
